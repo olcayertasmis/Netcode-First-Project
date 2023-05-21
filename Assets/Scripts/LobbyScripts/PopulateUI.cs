9346a18347cf36e148733064c7e11cebc46deda7
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-05-21 15:11:48 +0300

    Lobby Room - Edit
    Update the player's info text when enter the lobby -Added

--- Assets/Scripts/LobbyScripts/PopulateUI.cs
+++ Assets/Scripts/LobbyScripts/PopulateUI.cs
@@ -1,5 +1,8 @@
 using System;
 using TMPro;
+using Unity.Mathematics;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 namespace LobbyScripts
@@ -14,11 +17,20 @@ namespace LobbyScripts
         [SerializeField] private TextMeshProUGUI lobbyCode;
         [SerializeField] private TextMeshProUGUI gameMode;
 
+        [Header("Player Info")]
+        [SerializeField] private GameObject playerInfoPanel;
+        [SerializeField] private GameObject playerInfoPrefab;
+
+        private string _lobbyId;
+
         private void Start()
         {
             _currentLobby = GameObject.Find("Lobby Manager").GetComponent<CurrentLobby>();
 
             PopulateUIElements();
+
+            _lobbyId = _currentLobby.currentLobby.Id;
+            InvokeRepeating(nameof(PollForLobbyUpdate), 1.1f, 2f);
 
             LobbyStatic.LogPlayersInLobby(_currentLobby.currentLobby);
         }
@@ -28,6 +40,36 @@ namespace LobbyScripts
             lobbyName.text = "Lobby Name : " + _currentLobby.currentLobby.Name;
             lobbyCode.text = "Lobby Code : " + _currentLobby.currentLobby.LobbyCode;
             gameMode.text = "Game Mode : " + _currentLobby.currentLobby.Data["GameMode"].Value;
+
+            foreach (Player player in _currentLobby.currentLobby.Players)
+            {
+                CreatePlayerInfoCard(player);
+            }
+        }
+
+        private void CreatePlayerInfoCard(Player player)
+        {
+            var text = Instantiate(playerInfoPrefab, Vector3.zero, Quaternion.identity);
+            text.name = player.Joined.ToShortTimeString();
+            text.GetComponent<TextMeshProUGUI>().text = player.Id + " : " + player.Data["PlayerLevel"].Value;
+            text.transform.SetParent(playerInfoPanel.transform);
+        }
+
+        private async void PollForLobbyUpdate()
+        {
+            _currentLobby.currentLobby = await LobbyService.Instance.GetLobbyAsync(_lobbyId);
+            PopulateUIElements();
+        }
+
+        private void ClearContainer()
+        {
+            if (playerInfoPanel is not null && playerInfoPanel.transform.childCount > 0)
+            {
+                foreach (Transform VARIABLE in playerInfoPanel.transform)
+                {
+                    Destroy(VARIABLE.gameObject);
+                }
+            }
         }
     }
 }
